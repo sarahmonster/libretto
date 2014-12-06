1ba62d5dae67572d3d0dfa36d502fc4244c57bae
sarah semark <sarah@triggersandsparks.com>
2014-12-06 16:46:21 +0530

    Pare down top menu bar.

--- header.php
+++ header.php
@@ -47,7 +47,6 @@
 
 			<div class="site-branding">
 				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo('description'); ?></h2>
 			</div>
 
 			<nav id="site-navigation" class="navigation-main" role="navigation">

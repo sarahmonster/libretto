e0745cc2c399dff54de3377e1ace05a683125f60
sarah semark <sarah@triggersandsparks.com>
2014-12-15 16:38:42 +0530

    Style trackbacks and reply form headers.

--- comments.php
+++ comments.php
@@ -86,7 +86,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', '_readly' ); ?></p>
+		<h3 class="no-comments"><?php _e( 'Comments are now closed.', '_readly' ); ?></h3>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>

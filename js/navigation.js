b23993111d74ab7cc1d48b81aeaebfc330256c60
sarah <sarah@triggersandsparks.com>
2015-01-08 17:32:40 -0500

    If we load jQuery, maybe things will work as expected.

--- js/navigation.js
+++ js/navigation.js
@@ -8,7 +8,6 @@ jQuery(function() {
 
 	/* Drop menu down from top of screen */
 	jQuery('.menu-toggle').on('click', function() {
-		alert('Hello! .menu-toggle');
 		var menu = jQuery(this).parent().find('.menu');
 		menu.slideToggle('slow');
 		menu.toggleClass('menu-visible');
@@ -17,9 +16,7 @@ jQuery(function() {
 
 	/* Open sub-menus if we're using the teeny menu */
 	if (jQuery('.menu-toggle').is(':visible')) {
-		alert('Hello! .menu-toggle is active');
 		jQuery('.menu-item-has-children a').on('click', function() {
-			alert('Hello! .menu-toggle has been activated');
 			jQuery(this).parent().find('.sub-menu').slideToggle('fast');
 			return false;
 		});

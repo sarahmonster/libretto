c45b62fd867067ec93d33ac0d425c006c0327191
sarah <sarah@triggersandsparks.com>
2015-01-13 18:13:25 -1000

    Fix text domain issues.

--- comments.php
+++ comments.php
@@ -25,24 +25,24 @@ if ( post_password_required() ) {
 <?php if ( have_comments() ) : ?>
 	<h2 class="comments-title">
 	<?php
-	printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', '_readly' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+	printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'readly' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 	?>
 	</h2>
 
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Show comment navigation at top ?>
 		<?php global $cpage; // Get current comment page ?>
 		<nav id="comment-nav-above" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', '_readly' ); ?></h1>
+			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'readly' ); ?></h1>
 
 			<div class="previous">
 			<?php if ( 1 !== $cpage ): ?>
-				<span class="meta-nav"><?php previous_comments_link( __( 'Older Comments', '_readly' ) ); ?></span>
+				<span class="meta-nav"><?php previous_comments_link( __( 'Older Comments', 'readly' ) ); ?></span>
 			<?php endif; ?>
 			</div>
 
 			<div class="next">
 			<?php if ( $cpage ): ?>
-				<span class="meta-nav"><?php next_comments_link( __( 'Newer Comments', '_readly' ) ); ?></span>
+				<span class="meta-nav"><?php next_comments_link( __( 'Newer Comments', 'readly' ) ); ?></span>
 			<?php endif; ?>
 			</div>
 
@@ -62,17 +62,17 @@ if ( post_password_required() ) {
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Show comment navigation at bottom ?>
 		<?php global $cpage; // Get current comment page ?>
 		<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', '_readly' ); ?></h1>
+			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'readly' ); ?></h1>
 
 			<div class="previous">
 			<?php if ( 1 !== $cpage ): ?>
-				<span class="meta-nav"><?php previous_comments_link( __( 'Older Comments', '_readly' ) ); ?></span>
+				<span class="meta-nav"><?php previous_comments_link( __( 'Older Comments', 'readly' ) ); ?></span>
 			<?php endif; ?>
 			</div>
 
 			<div class="next">
 			<?php if ( $cpage ): ?>
-				<span class="meta-nav"><?php next_comments_link( __( 'Newer Comments', '_readly' ) ); ?></span>
+				<span class="meta-nav"><?php next_comments_link( __( 'Newer Comments', 'readly' ) ); ?></span>
 			<?php endif; ?>
 			</div>
 
@@ -84,7 +84,7 @@ if ( post_password_required() ) {
 <?php // If comments are closed and there are comments, let's leave a little note, shall we?
 if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 ?>
-	<h3 class="no-comments"><?php _e( 'Comments are now closed.', '_readly' ); ?></h3>
+	<h3 class="no-comments"><?php _e( 'Comments are now closed.', 'readly' ); ?></h3>
 <?php endif; ?>
 
 <?php comment_form(); ?>
--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
 			<div class="site-info">
-				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'ryu' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'readly' ), 'WordPress' ); ?></a>
+				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'readly' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'readly' ), 'WordPress' ); ?></a>
 					<?php printf( __( 'Theme: %1$s by %2$s.', 'readly' ), 'Readly', '<a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>' ); ?>
 			</div><!-- .site-info -->
 
@@ -21,7 +21,7 @@
 						'theme_location' => 'social',
 					 	'link_before'    => '<span class="screen-reader-text">',
 						'link_after'      => '</span>',
-					 	'fallck_cb' => false,
+					 	'fallback_cb' => false,
 					) );
 				 	?>
 				</div><!-- #social -->

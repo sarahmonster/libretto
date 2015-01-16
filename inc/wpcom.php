df28bb436f89215641fc96a1c86c626bf5a00c9e
sarah <sarah@triggersandsparks.com>
2015-01-16 00:20:08 -1000

    Add wpcom.php file for WordPress.com compatibility.

---
+++ inc/wpcom.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* WordPress.com-specific functions and definitions.
+*
+* This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
+*
+* @package Readly
+*/
+
+
+/**
+* Adds support for wp.com-specific theme functions.
+*
+* @global array $themecolors
+*/
+function readly_wpcom_setup() {
+  global $themecolors;
+  // Set theme colors for third party services.
+  if ( ! isset( $themecolors ) ):
+    $themecolors = array(
+      'bg'     => 'eae9e6',
+      'text'   => '363431',
+      'link'   => '932817',
+      'border' => 'd9d6d0',
+      'url'    => '787065',
+    );
+  endif;
+}
+add_action( 'after_setup_theme', 'readly_wpcom_setup' );
+
+
+/*
+* Add default print styles. TODO: Add proper print styles.
+*/
+add_theme_support( 'print-style' );

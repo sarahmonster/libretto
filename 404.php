c027456654702cb39be79397aeffb529810cbf20
sarah <sarah@triggersandsparks.com>
2015-01-23 00:00:19 -0500

    Add widgets and standardized text to 404 page.

--- 404.php
+++ 404.php
@@ -12,8 +12,35 @@ get_header();
 
 		<article id="post-0" class="post error404 not-found">
 			<div class="entry-content">
-				<p><?php _e( 'Looks like something got lost along the way. Please use the navigation above or the search box below to find what you were looking for.', 'readly' ); ?></p>
+				<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'readly' ); ?></p>
 				<?php get_search_form(); ?>
+
+        <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+
+          <?php if ( readly_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+          <div class="widget widget_categories">
+            <h2 class="widgettitle"><?php _e( 'Most Used Categories', 'readly' ); ?></h2>
+            <ul>
+            <?php
+              wp_list_categories( array(
+                'orderby'    => 'count',
+                'order'      => 'DESC',
+                'show_count' => 1,
+                'title_li'   => '',
+                'number'     => 10,
+              ) );
+            ?>
+            </ul>
+          </div><!-- .widget -->
+          <?php endif; ?>
+
+          <?php
+            /* translators: %1$s: smiley */
+            $archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'readly' ), convert_smilies( ':)' ) ) . '</p>';
+            the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+          ?>
+
+          <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 			</div><!-- .entry-content -->
 		</article><!-- #post-0 .post .error404 .not-found -->
 
